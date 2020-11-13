efec3f270d0a09a669a929ed7c0083d567f8acd2
Toan Dang <toan.dang@zimplistic.com>
2020-11-13 17:38:46 +0800

    Init commit

---
+++ noclue.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+
+int main(int argv, char *argc[]) {
+	std::cout << "You totally " << std::pow(atoi(argc[1]), 3) << " time have no clue!" << std::endl;
+
+	return 0;
+}
